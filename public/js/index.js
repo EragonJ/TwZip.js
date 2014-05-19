f2b25654615a2b8a5fe6c76554ecde59b040d0c1
EragonJ <eragonj@eragonj.me>
2014-05-19 23:28:43 +0800

    make a real example !

--- public/js/index.js
+++ public/js/index.js
@@ -3,9 +3,33 @@ $(document).ready(function() {
     dataOrigin: './data/db/'
   }); 
 
-  twZip.search('', function(data) {
-    console.log(data);
+  // default
+  search('', 1);
+
+  // bind events
+  $('#select1').on('change', function() {
+    var searchWords = $(this).val();
+    search(searchWords, 2); 
   });
+
+  $('#select2').on('change', function() {
+    var searchWords = $('#select1').val() + $(this).val();
+    search(searchWords, 3); 
+  });
+
+  function search(words, nextLayer) {
+    twZip.search(words, function(data) {
+      var optionDOM;
+      var options = [];
+      Object.keys(data).forEach(function(key) {
+        optionDOM = document.createElement('option');
+        optionDOM.textContent = key;
+        options.push(optionDOM);
+      });
+
+      $('#select' + nextLayer).empty().append(options);
+    });
+  }
 
   window.twZip = twZip;
 });
