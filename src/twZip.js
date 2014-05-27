917bd7798a7febdc3d4986a5e43a5fee4650902b
EragonJ <eragonj@eragonj.me>
2014-05-27 22:16:21 +0800

    right now we can search more than 1 layer at the same time

--- src/twZip.js
+++ src/twZip.js
@@ -39,7 +39,6 @@
   var TwZip = function(userOptions) {
     this.dataOrigin = userOptions.dataOrigin;
     this.summarySuffix = 'summary.json';
-
     this._init();
   };
 
@@ -52,33 +51,14 @@
       this._reField[1] = /([鄉市鎮區台]|群島)/;
       this._reField[2] = /([路街巷村段台]|市場)/;
     },
-    _fetchData: function(zipFields, cb) {
+    _fetchData: function(zipFields) {
       var self = this;
 
       // we would fetch the summary
       zipFields.push(this.summarySuffix);
-      
-      $.ajax({
-        url: this.dataOrigin + zipFields.join('/')
-      }).done(function(data) {
-        var summary = data.summary;
-        summary = self._changeDataToObject(summary);
 
-        if (zipFields.length === 1) {
-          self._zipCache = summary;
-        }
-        else if (zipFields.length === 2) {
-          self._zipCache[zipFields[0]] = summary;
-        }
-        else if (zipFields.length === 3) {
-          self._zipCache[zipFields[0]][zipFields[1]] = summary;
-        }
-        else {
-          self._zipCache[zipFields[0]][zipFields[1]][zipFields[2]] = summary;
-        }
-
-        cb(summary); 
-      });
+      var deferred = $.getJSON(this.dataOrigin + zipFields.join('/'));
+      return deferred;
     },
     /*
      *  We would try to tokenize userInput to get fields.
@@ -134,18 +114,21 @@
       }
       else if (zipFields.length === 1) {
         if (!this._zipCache[zipFields[0]]) {
-          return false; 
+          return false;
         }
         return this._zipCache[zipFields[0]];
       }
       else if (zipFields.length === 2) {
-        if (!this._zipCache[zipFields[0]][zipFields[1]]) {
-          return false;
+        if (!this._zipCache[zipFields[0]] ||
+          !this._zipCache[zipFields[0]][zipFields[1]]) {
+            return false;
         }
         return this._zipCache[zipFields[0]][zipFields[1]];
       }
       else {
-        if (!this._zipCache[zipFields[0]][zipFields[1]][zipFields[2]]) {
+        if (!this._zipCache[zipFields[0]] ||
+          !this._zipCache[zipFields[0]][zipFields[1]] ||
+          !this._zipCache[zipFields[0]][zipFields[1]][zipFields[2]]) {
           return false;
         }
         return this._zipCache[zipFields[0]][zipFields[1]][zipFields[2]];
@@ -158,20 +141,84 @@
       });
       return obj;
     },
+    _storeSummaryIntoCache: function(zipFields, data) {
+      var summary = data.summary;
+      summary = this._changeDataToObject(summary);
+
+      console.log(zipFields);
+      if (zipFields.length === 0) {
+        this._zipCache = summary;
+      }
+      else if (zipFields.length === 1) {
+        this._zipCache = summary;
+      }
+      else if (zipFields.length === 2) {
+        this._zipCache[zipFields[0]] = summary;
+      }
+      else if (zipFields.length === 3) {
+        this._zipCache[zipFields[0]][zipFields[1]] = summary;
+      }
+      else {
+        this._zipCache[zipFields[0]][zipFields[1]][zipFields[2]] = summary;
+      }
+    },
     search: function(userInput, cb) {
       var zipFields;
       var cacheZip;
+      var self;
+      var deferredList = [];
+      var partOfZipcodeList = [];
       cb = cb || function() {};
 
       userInput = this._transformCommonWords(userInput);
       zipFields = this._processUserInput(userInput);
-      cacheZip = this._inCache(zipFields);
 
-      if (cacheZip) {
-        cb(cacheZip);
+      // twZip.search('');
+      if (zipFields.length === 0) {
+        if (!this._inCache([])) {
+          var deferred = this._fetchData([]);
+          deferredList.push(deferred);
+          partOfZipcodeList.push([]);
+        }
+      }
+      // twZip.search('台北市信義區信義路')
+      else {
+        for (var i = 0; i < zipFields.length; i++) {
+          // ['台北市', '信義區', '信義路'] would be
+          // ['台北市']
+          // ['台北市', '信義區']
+          // ['台北市', '信義區', '信義路']
+          var partOfZipcode = zipFields.slice(0, i + 1);
+          partOfZipcodeList.push(partOfZipcode);
+
+          if (!this._inCache(partOfZipcode)) {
+            var deferred = this._fetchData(partOfZipcode);
+            deferredList.push(deferred);
+          }
+        }
+      }
+
+      if (deferredList.length > 0) {
+        var defer = $.when.apply($, deferredList);
+        defer.done(function() {
+          // there are two differnt behaviors to `done` method
+          if (deferredList.length === 1) {
+            this._storeSummaryIntoCache(partOfZipcodeList[0],
+              arguments[0]);
+          }
+          else {
+            $.each(arguments, function(index, eachRequest) {
+              if (eachRequest && eachRequest[0]) {
+                this._storeSummaryIntoCache(partOfZipcodeList[index],
+                  eachRequest[0]);
+              }
+            }.bind(this));
+          }
+          cb(this._inCache(zipFields));
+        }.bind(this));
       }
       else {
-        this._fetchData(zipFields, cb);
+        cb(this._inCache(zipFields));
       }
     }
   };
